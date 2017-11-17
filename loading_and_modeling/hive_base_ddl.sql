b058bd9b8d3bec3ac732500094de3ae06586c668
w205 <w205@ip-172-31-86-1.ec2.internal>
2017-11-17 04:17:59 +0000

    Adding hive_base_ddl.sql

---
+++ loading_and_modeling/hive_base_ddl.sql
@@ -0,0 +1,166 @@
+DROP TABLE insta_products;
+
+CREATE EXTERNAL TABLE insta_products
+(
+product_id string,
+product_name string,
+aisle_id string,
+department_id string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/insta_products';
+
+
+DROP TABLE insta_products_train;
+
+CREATE EXTERNAL TABLE insta_products_train
+(
+order_id string,
+product_id string,
+add_to_cart_order string,
+reordered string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/insta_order_products_train';
+
+
+DROP TABLE insta_products_prior;
+
+CREATE EXTERNAL TABLE insta_products_prior
+(
+order_id string,
+product_id string,
+add_to_cart_order string,
+reordered string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/insta_order_products_prior';
+
+
+DROP TABLE insta_orders;
+
+CREATE EXTERNAL TABLE insta_orders
+(
+order_id string,
+user_id string,
+eval_set string,
+order_number string,
+order_dow string,
+order_hour_of_day string,
+days_since_prior string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/insta_orders';
+
+
+DROP TABLE insta_dept;
+
+CREATE EXTERNAL TABLE insta_dept
+(
+department_id string,
+department string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/insta_departments';
+
+DROP TABLE insta_aisles;
+
+CREATE EXTERNAL TABLE insta_aisles
+(
+aisle_id string,
+aisle string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/insta_aisles';
+
+DROP TABLE d_trans;
+
+CREATE EXTERNAL TABLE d_trans
+(
+household_key string,
+basket_id string,
+day string,
+product_id string,
+quantity string,
+sales_value string,
+store_id string,
+coupon_match_disc string,
+coupon_disc string,
+retail_disc string,
+trans_time string,
+week_no string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/dh_transaction_data';
+
+DROP TABLE d_product;
+
+CREATE EXTERNAL TABLE d_product
+(
+product_id string,
+department string,
+commodity_desc string,
+sub_commodity_desc string,
+manufacturer string,
+brand string,
+curr_size_of_product string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+
+STORED AS TEXTFILE
+LOCATION '/user/w205/grocery_shopper_classifier/dh_product';
+
