6f2d9917821e8373803d35f955f82e4cb744dfd6
noahrandolph <noah.randolph@gmail.com>
2017-12-14 23:53:29 +0000

    fixed ML directory for Python in Run_Application.sh

--- Run_Application.sh
+++ Run_Application.sh
@@ -23,8 +23,14 @@ hive -f ./transforming/order_data_combine_ddl.sql
 # create auxiliary (non-1-hot encoded) table for EDA in Tableau
 hive -f ./visualization/aux_for_tableau_ddl.sql
 
+# change to machine_learning directory
+cd /data/W205_Final_Project/machine_learning
+
 # import tables and training & test machine learning model, also export .csv of results
-python3 ./machine_learning/gs_classifier.py
+python3 ./gs_classifier.py
+
+# change back to main directory
+cd /data/W205_Final_Project
 
 # create SQL table for visualization of classifier results
 ./visualization/load_test_results.sh
